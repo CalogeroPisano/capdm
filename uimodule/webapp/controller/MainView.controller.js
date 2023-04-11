952a2e7a6221f7e5a1f93cd76c784bccff75f5cf
CalogeroPisano <calogero.pisano@capgemini.com>
2023-04-11 13:00:49 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["capgemini/CapgeminiPOD/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("capgemini.CapgeminiPOD.controller.MainView", {});
+});
---
+++ uimodule/webapp/test/integration/arrangements/Startup.js
@@ -0,0 +1,19 @@
+sap.ui.define(["sap/ui/test/Opa5"],
+    /**
+     * @param {typeof sap.ui.test.Opa5} Opa5 
+     */
+    function (Opa5) {
+        "use strict";
+
+        return Opa5.extend("capgemini.CapgeminiPOD.test.integration.arrangements.Startup", {
+            iStartMyApp: function () {
+                this.iStartMyUIComponent({
+                    componentConfig: {
+                        name: "capgemini.CapgeminiPOD",
+                        async: true,
+                        manifest: true
+                    }
+                });
+            }
+        });
+    });
